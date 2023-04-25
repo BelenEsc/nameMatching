dc18ba66664a6a32827710b2b57cc05224fe2e4f
BelenEsc <belenescobari@gmail.com>
2023-04-25 16:48:34 +0200

    bgbm

--- namematching/src/namematching/AuthorNamesAbrev.java
+++ namematching/src/namematching/AuthorNamesAbrev.java
@@ -1,9 +1,11 @@
 package namematching;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -11,17 +13,24 @@ import org.w3c.dom.NodeList;
 
 public class AuthorNamesAbrev {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		String input = "Andrew A. Burbidge";
-		String pathToXML="C:/Users/belen/git/nameMatching/namematching/src/namematching/AuthorNamesAbrev.xml"; 
+		String input = "Charles Darwin";
+		String pathToXML= System.getProperty("user.dir");
+		String xmlAbrevNames = pathToXML+"/src/namematching/AuthorNamesAbrev.xml";
+		String result = nameAbbreviation(input, xmlAbrevNames);
+		System.out.println(result);
+	}
+
+	private static String nameAbbreviation(String input, String xmlAbrevNames) {
+		
 		try {
-			File xmlFile = new File(pathToXML);
+			File xmlFile = new File(xmlAbrevNames);
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(xmlFile);
 			doc.getDocumentElement().normalize();
-
+			
 			NodeList nodeList = doc.getElementsByTagName("replacement");
 			String[][] replacements = new String[nodeList.getLength()][2];
 
@@ -37,13 +46,12 @@ public class AuthorNamesAbrev {
 			}
 
 			for (String[] replacement : replacements) {
-				input = input.replaceAll(replacement[0], replacement[1]);
+				input = input.replaceAll(replacement[0], replacement[1].toLowerCase());
 			}
-
-			System.out.println("Replaced string: " + input);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return input;
 	}
 }
